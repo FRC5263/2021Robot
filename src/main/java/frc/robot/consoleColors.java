4a167a52fdc3de0f472706ed04f1bbe89f39a1ab
guyish1 <piercewlangford@outlook.com>
2021-04-23 16:34:12 -0400

    added class for output formating

---
+++ src/main/java/frc/robot/consoleColors.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** use any variable in this class to spice up the terminal output
+ * remember to use RESET to reset the formating otherwise it will affect all other cosole outputs
+*/
+public class consoleColors {
+    
+}
