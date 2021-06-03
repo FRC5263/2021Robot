d473dc6ebb72a6dbfc3d3a47e1d176042f307b0f
guyish1 <piercewlangford@outlook.com>
2021-06-03 09:36:57 -0400

    fixes

--- src/main/java/frc/robot/robotsV3.java
+++ src/main/java/frc/robot/robotsV3.java
@@ -14,32 +14,28 @@ import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.armSubsystem;
 import frc.robot.subsystems.intakeSubsystem;
 import frc.robot.subsystems.shooterSubsystem;
-import frc.robot.subsystems.CheckDIO;
 
 /** Add your docs here. */
 public class robotsV3 extends Robot{
+    static DigitalInput dio9 = new DigitalInput(9);
+    static DigitalInput dio8 = new DigitalInput(8);
 
     /*
-    public static robotObject createSeniorTeam() {
-        return new robotObject(
-            new HashMap<String, Subsystem>() {
-                {put(robotObject.DRIVETRAIN, new DriveTrainSubsystem(new PWMTalonSRX(1), new PWMTalonSRX(1)));}
-                {put(robotObject.SHOOTER, new shooterSubsystem(new PWMTalonSRX(1)));}
-                {put(robotObject.INTAKE, new intakeSubsystem(new PWMTalonSRX(1)));}
-            }
-        );
-    }
-    */
-
+     * public static robotObject createSeniorTeam() { return new robotObject( new
+     * HashMap<String, Subsystem>() { {put(robotObject.DRIVETRAIN, new
+     * DriveTrainSubsystem(new PWMTalonSRX(1), new PWMTalonSRX(1)));}
+     * {put(robotObject.SHOOTER, new shooterSubsystem(new PWMTalonSRX(1)));}
+     * {put(robotObject.INTAKE, new intakeSubsystem(new PWMTalonSRX(1)));} } ); }
+     */
 
     public static robotControl getBot() {
-        if (CheckDIO.CheckDIO(8) == true) {
+        if (dio9.get() == false) {
             System.out.print("running on dio 9 config");
-            return null;
-        } else if (CheckDIO.CheckDIO(9) == true) {
+            return new robotControl(new DriveTrainSubsystem(new Spark(8), new Spark(0)));
+        } else if (dio8.get() == false) {
             System.out.print("running on dio 9 config");
             return new robotControl(new DriveTrainSubsystem(new WPI_TalonSRX(7), new WPI_VictorSPX(10)), new armSubsystem(new Spark(8), new Spark(9)), new shooterSubsystem(new WPI_TalonSRX(5), new WPI_TalonSRX(2), new WPI_TalonSRX(4)), true);
-        } else if (CheckDIO.CheckDIO(8) == false && (CheckDIO.CheckDIO(9) == false)) {
+        } else if (dio8.get() == true && (dio9.get() == true)) {
             System.out.print("not a recognised robot");
             return null;
         } else {
